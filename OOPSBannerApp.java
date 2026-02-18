04705586eebd51c80b19c294202c9e5288910227
sindhurmarella1835@gmail.com <sindhurmarella123@gmail.com>
2026-02-18 16:39:32 +0530

    Coded UC4 to Print OOPS to the console using String Arrays

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -3,21 +3,28 @@ OOPSBannerApp UC3 - OOPS Banner Display Application
 
 This class demonstrates the creation of a visual banner displaying the word "OOPS" with the help of .join()method.
 @author Developer
-@version 3
+@version 4
 */
 
 
-public class OOPSBannerAppUC3{
+
+public class OOPSBannerApp{
 	// Main method to run the banner display
 	public static void main(String[] args){
 		//String.join() method is used to concatenate the strings.
-		System.out.println(String.join(" ", "   ***   "  , "   ***   "  , "****** "  , "   ******"));
-		System.out.println(String.join(" ", " **   ** "  , " **   ** "  , "**   **"  , "  ***    "));
-		System.out.println(String.join(" ", " **   ** "  , " **   ** "  , "**   **"  , "  ***    "));
-		System.out.println(String.join(" ", " **   ** "  , " **   ** "  , "****** "  , "    ***  "));
-		System.out.println(String.join(" ", " **   ** "  , " **   ** "  , "**     "  , "      ***"));
-		System.out.println(String.join(" ", " **   ** "  , " **   ** "  , "**     "  , "      ***"));
-		System.out.println(String.join(" ", " **   ** "  , " **   ** "  , "**     "  , "    ***  "));
-		System.out.println(String.join(" ", "   ***   "  , "   ***   "  , "**     "  , " *****   "));
+		String[] banner = new String[8];
+		//The banner[i] represents one row in O, O, P, S
+		banner[0] = String.join(" | ", "   ***   "  , "   ***   "  , " ******  "  , "   ****** ");
+		banner[1] = String.join(" | ", " **   ** "  , " **   ** "  , " **   ** "  , "  ***     ");
+		banner[2] = String.join(" | ", " **   ** "  , " **   ** "  , " **   ** "  , "  ***     ");
+		banner[3] = String.join(" | ", " **   ** "  , " **   ** "  , " ******  "  , "    ***   ");
+		banner[4] = String.join(" | ", " **   ** "  , " **   ** "  , " **      "  , "      *** ");
+		banner[5] = String.join(" | ", " **   ** "  , " **   ** "  , " **      "  , "      *** ");
+		banner[6] = String.join(" | ", " **   ** "  , " **   ** "  , " **      "  , "    ***   ");
+		banner[7] = String.join(" | ", "   ***   "  , "   ***   "  , " **      "  , " *****    ");
+		// For loop to print the whole banner line by line
+		for(int i = 0; i < banner.length; i++){
+			System.out.println(banner[i]);
+		}
 	}
 }
