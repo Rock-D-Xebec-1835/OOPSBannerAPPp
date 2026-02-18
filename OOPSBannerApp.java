3d480491d0b09e3b59cfe69af8ed61c5dc5657cb
sindhurmarella1835@gmail.com <sindhurmarella123@gmail.com>
2026-02-18 15:58:06 +0530

    Coded UC2 to Print OOPS to console using asterisks

---
+++ OOPSBannerApp.java
@@ -0,0 +1,23 @@
+/*
+OOPSBannerApp UC2 - OOPS Banner Display Application
+
+This class demonstrates the creation of a visual banner displaying the word "OOPS" composed of Asterisks(*)
+@author Developer
+@version 2
+*/
+
+
+public class OOPSBannerAppUC2{
+	// Main method to run the banner display
+	public static void main(String[] args){
+	//Each line corresponds to a row in banner for letters O, O, P, S
+		System.out.println("   ***   " + " " + "   ***   " + " " + "****** " + " " + "   ******");
+		System.out.println(" **   ** " + " " + " **   ** " + " " + "**   **" + " " + "  ***    ");
+		System.out.println(" **   ** " + " " + " **   ** " + " " + "**   **" + " " + "  ***    ");
+		System.out.println(" **   ** " + " " + " **   ** " + " " + "****** " + " " + "    ***  ");
+		System.out.println(" **   ** " + " " + " **   ** " + " " + "**     " + " " + "      ***");
+		System.out.println(" **   ** " + " " + " **   ** " + " " + "**     " + " " + "      ***");
+		System.out.println(" **   ** " + " " + " **   ** " + " " + "**     " + " " + "    ***  ");
+		System.out.println("   ***   " + " " + "   ***   " + " " + "**     " + " " + " *****   ");
+	}
+}
